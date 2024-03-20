454d3f217c3cbf93ee33618c72a9e693898853ca
Luiz Gustavo Zeglan <89813478+GustavoZeglan@users.noreply.github.com>
2024-03-19 22:15:29 -0300

    Delete firebaseAccount.config.ts

--- firebaseAccount.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseAccountConfig = {
-  apiKey: 'AIzaSyDPpgoI_kJGwnc4Bj5iqBLg6Sx3Tb_iZBM',
-  authDomain: 'midia-books.firebaseapp.com',
-  projectId: 'midia-books',
-  storageBucket: 'midia-books.appspot.com',
-  messagingSenderId: '425128531594',
-  appId: '1:425128531594:web:640e4855df257661153f73',
-  measurementId: 'G-1ELSHBWPXY',
-};
